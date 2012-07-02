ef6870dee7e3f89e69c852264bfa37ab028807e5
Eduardo Maia <eduardo@zigotto.com.br>
2012-07-02 16:52:42 -0300

    added url functionality into banners_helpers

--- app/helpers/spree/banners_helper.rb
+++ app/helpers/spree/banners_helper.rb
@@ -8,18 +8,24 @@ module Spree
       cl = params[:class] || "banner"
       # style items show
       style = params[:style] || "list"
-      banner = Banner.enable(category).limit(max)
-      if !banner.blank?
-        banner = banner.sort_by { |ban| ban.position }
+
+      banner = Banner.enabled.limit(max)
+      unless banner.blank?
+        # random order
+        banner = banner.sort_by{rand}
 
         if (style == "list")
-          content_tag(:ul, raw(banner.map do |ban| content_tag(:li, link_to(image_tag(ban.attachment.url(:custom)), ban.url), :class => cl) end.join) )
+          content_tag(:ul, raw(banner.map do |ban| content_tag(:li, link_to(image_tag(ban.attachment.url(image_size.to_sym)), ban.url), :class => cl) end.join) )
         else
-          raw(banner.map do |ban| content_tag(style.to_sym, link_to(image_tag(ban.attachment.url(:custom)), ban.url), :class => cl) end.join)
+          raw(banner.map do |ban|
+            if ban.url?
+              content_tag(style.to_sym, link_to(image_tag(ban.attachment.url(:custom)), ban.url), :class => cl)
+            else
+              content_tag(style.to_sym, image_tag(ban.attachment.url(:custom)), :class => cl)
+            end
+          end.join)
         end
-
       end
     end
-
   end
 end
